e88a1b3cf4304287c0fee3355aef3708b96789ed
Your Name <you@example.com>
2022-05-20 18:24:53 +0530

    Created Employee Payroll App

---
+++ src/main/java/com/example/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.employeepayrollapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeePayrollAppApplication.class, args);
+        System.out.println("Welcome to Employee Payroll App");
+    }
+
+}
