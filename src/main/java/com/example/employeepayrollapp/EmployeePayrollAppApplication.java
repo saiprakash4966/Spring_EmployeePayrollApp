e064cf955011857f7c9205c1f3483e7534cf3998
Your Name <you@example.com>
2022-05-24 09:44:17 +0530

    Using Lombok library for logging

--- src/main/java/com/example/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/example/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -1,16 +1,17 @@
 package com.example.employeepayrollapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class EmployeePayrollAppApplication
-{
+@Slf4j
+public class EmployeePayrollAppApplication {
 
-    public static void main(String[] args)
-    {
-        SpringApplication.run(EmployeePayrollAppApplication.class, args);
-        System.out.println("Welcome to Employee Payroll App");
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = SpringApplication.run(EmployeePayrollAppApplication.class, args);
+        log.info("Employee Payroll App started in {} environment",
+                applicationContext.getEnvironment().getProperty("environment"));
     }
-
 }
