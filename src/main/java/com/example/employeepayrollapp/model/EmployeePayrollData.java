c289435779405ad60839405d377c9fbd91d8a82b
Your Name <you@example.com>
2022-05-24 09:29:49 +0530

    Created Lombok Library for auto generate getter and setters method

--- src/main/java/com/example/employeepayrollapp/model/EmployeePayrollData.java
+++ src/main/java/com/example/employeepayrollapp/model/EmployeePayrollData.java
@@ -1,9 +1,10 @@
 package com.example.employeepayrollapp.model;
 
 import com.example.employeepayrollapp.dto.EmployeePayrollDTO;
+import lombok.Data;
 
-
-public class EmployeePayrollData {
+public @Data
+class EmployeePayrollData {
 
     private int employeeId;
     private String name;
@@ -16,29 +17,5 @@ public class EmployeePayrollData {
         this.employeeId = employeeId;
         this.name = employeePayrollDTO.name;
         this.salary = employeePayrollDTO.salary;
-    }
-
-    public int getEmployeeId() {
-        return employeeId;
-    }
-
-    public void setEmployeeId(int employeeId) {
-        this.employeeId = employeeId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public long getSalary() {
-        return salary;
-    }
-
-    public void setSalary(long salary) {
-        this.salary = salary;
     }
 }
