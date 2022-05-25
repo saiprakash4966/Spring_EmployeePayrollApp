9048a2e921ab56766922b4aab1ebc7afc3b0ae63
Your Name <you@example.com>
2022-05-25 09:53:05 +0530

    Saving data to mysql database

--- src/main/java/com/example/employeepayrollapp/model/EmployeePayrollData.java
+++ src/main/java/com/example/employeepayrollapp/model/EmployeePayrollData.java
@@ -3,34 +3,56 @@ package com.example.employeepayrollapp.model;
 import com.example.employeepayrollapp.dto.EmployeePayrollDTO;
 import lombok.Data;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.List;
 
-public @Data
-class EmployeePayrollData {
+@Entity
+@Table(name = "employee_payroll")
+public @Data class EmployeePayrollData {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "employee_id")
     private int employeeId;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "salary")
     private long salary;
+
+    @Column(name = "gender")
     public String gender;
+
+    @Column(name = "start_date")
     public LocalDate startDate;
+
+    @Column(name = "note")
     public String note;
-    public String ProfilePic;
+
+    @Column(name = "profile_pic")
+    public String profilePic;
+
+    @ElementCollection
+    @CollectionTable(name = "employee_department", joinColumns = @JoinColumn(name = "id"))
+    @Column(name = "department")
     public List<String> departments;
-
-
 
     public EmployeePayrollData() {
     }
 
-    public EmployeePayrollData(int employeeId, EmployeePayrollDTO employeePayrollDTO) {
-        this.employeeId = employeeId;
+    public EmployeePayrollData(EmployeePayrollDTO employeePayrollDTO) {
+        this.updateEmployeePayrollData(employeePayrollDTO);
+    }
+
+    private void updateEmployeePayrollData(EmployeePayrollDTO employeePayrollDTO) {
         this.name = employeePayrollDTO.name;
         this.salary = employeePayrollDTO.salary;
         this.gender = employeePayrollDTO.gender;
         this.startDate = employeePayrollDTO.startDate;
         this.note = employeePayrollDTO.note;
-        this.ProfilePic = employeePayrollDTO.profilePic;
-        this.departments =employeePayrollDTO.departments;
+        this.profilePic = employeePayrollDTO.profilePic;
+        this.departments = employeePayrollDTO.departments;
     }
 }
--- src/main/java/com/example/employeepayrollapp/service/EmployeePayrollService.java
+++ src/main/java/com/example/employeepayrollapp/service/EmployeePayrollService.java
@@ -3,15 +3,22 @@ package com.example.employeepayrollapp.service;
 import com.example.employeepayrollapp.dto.EmployeePayrollDTO;
 import com.example.employeepayrollapp.exceptions.EmployeePayrollCustomException;
 import com.example.employeepayrollapp.model.EmployeePayrollData;
+import com.example.employeepayrollapp.repository.EmployeePayrollRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
 public class EmployeePayrollService implements IEmployeePayrollService{
 
     private List<EmployeePayrollData> employeePayrollDataList = new ArrayList<>();
+    @Autowired
+    private EmployeePayrollRepository employeePayrollRepository;
+
 
     @Override
     public List<EmployeePayrollData> getEmployeePayrollData() {
@@ -30,9 +37,10 @@ public class EmployeePayrollService implements IEmployeePayrollService{
     @Override
     public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO employeePayrollDTO) {
         EmployeePayrollData employeePayrollData = null;
-        employeePayrollData = new EmployeePayrollData(employeePayrollDataList.size() + 1, employeePayrollDTO);
+        employeePayrollData = new EmployeePayrollData(employeePayrollDTO);
         employeePayrollDataList.add(employeePayrollData);
-        return employeePayrollData;
+        log.debug("Employee data : " + employeePayrollData.toString());
+        return employeePayrollRepository.save(employeePayrollData);
     }
 
     @Override
